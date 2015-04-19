2b1b22f4988d818eedee563bc675501dd5507eee
Brian Clanton <brian.clanton@gmail.com>
2015-04-18 20:46:38 -0400

    The app now responds to you :D

--- request-parser.js
+++ request-parser.js
@@ -1,4 +1,16 @@
 var parseRequest = function(client, request) {
+  client.sendMessage({
+    to: request.From,
+    body: "You said \"" + request.Body + "\"",
+    from: process.env.TWILIO_NUMBER
+  }, function(err, messageData) {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log("Message sent! SID: " + messageData.sid);
+    }
+  });
+
   return "Message from " + request.From + ": " + request.Body;
 }
 
