41c9c94c84581e254d55763ac9f2ff95023265a7
Andrew Hollenbach <hollenbach.andrew@gmail.com>
2015-04-18 20:57:29 -0400

    Test song info

--- request-parser.js
+++ request-parser.js
@@ -14,4 +14,22 @@ var parseRequest = function(client, request) {
   return "Message from " + request.From + ": " + request.Body;
 }
 
+var getSongInfo = function() {
+    // don't forget to to npm install echojs
+    // and export your variable!
+    var echojs = require('echojs');
+
+    var echo = echojs({
+      key: process.env.ECHONEST_KEY
+    });
+
+    // http://developer.echonest.com/docs/v4/song.html#search
+    echo('song/search').get({
+      artist: 'radiohead',
+      title: 'karma police'
+    }, function (err, json) {
+      console.log(json.response);
+    });
+}
+
 module.exports = parseRequest
